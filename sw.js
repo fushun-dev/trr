5963999decb3f25da22e092ad5e1f73d41517c31
fushun-dev <fushun92@gmail.com>
2026-06-07 08:58:59 +0000

    Scale hero title down on mobile; SW v19

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Taiwan Rice Roll PWA service worker.
    Network-first for HTML/CSS/JS (always fresh), cache-first for other assets. */
-const CACHE = 'trr-v18';
+const CACHE = 'trr-v19';
 const ASSETS = [
   './',
   './index.html',
